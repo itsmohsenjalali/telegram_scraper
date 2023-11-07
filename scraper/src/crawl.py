b9a737c742167c139419c2aa49b82b5920a8d969
itsmohsenjalali <mo.jalali90@gmail.com>
2023-11-07 12:48:54 +0330

    cacth exeptions in crwaling

--- scraper/src/crawl.py
+++ scraper/src/crawl.py
@@ -12,42 +12,45 @@ def get_conversations(client: TelegramClient):
         phone_number=client.get_me().phone)
     with alive_bar() as bar:
         for conversation in conversations:
-            if isinstance(conversation, Chat):
-                if hasattr(conversation.entity, 'megagroup'):
-                    if conversation.entity.gigagroup == True:
+            try:
+                if isinstance(conversation, Chat):
+                    if hasattr(conversation.entity, 'megagroup'):
+                        if conversation.entity.gigagroup == True:
+                            continue
+                        TelegramGroup.objects.update_or_create(
+                            id=conversation.id,
+                            defaults={
+                                'title': conversation.title,
+                                'is_super_group': True,
+                                'participants_count': conversation.entity.participants_count
+                            }
+                        )
+                    else:
+                        TelegramGroup.objects.update_or_create(
+                            id=conversation.id,
+                            defaults={
+                                'title': conversation.title,
+                                'is_super_group': False,
+                                'participants_count': conversation.entity.participants_count
+                            }
+                        )
+                    telegram_account.groups.add(conversation.id)
+                elif isinstance(conversation, User):
+                    if conversation.entity.bot == True:
                         continue
-                    TelegramGroup.objects.update_or_create(
-                        id=conversation.id,
+                    TelegramUser.objects.update_or_create(
+                        id=conversation.entity.id,
                         defaults={
-                            'title': conversation.title,
-                            'is_super_group': True,
-                            'participants_count': conversation.entity.participants_count
+                            'username': conversation.entity.username,
+                            'first_name': conversation.entity.first_name,
+                            'last_name': conversation.entity.last_name,
+                            'phone_number': conversation.entity.phone
                         }
                     )
-                else:
-                    TelegramGroup.objects.update_or_create(
-                        id=conversation.id,
-                        defaults={
-                            'title': conversation.title,
-                            'is_super_group': False,
-                            'participants_count': conversation.entity.participants_count
-                        }
-                    )
-                telegram_account.groups.add(conversation.id)
-            elif isinstance(conversation, User):
-                if conversation.entity.bot == True:
-                    continue
-                TelegramUser.objects.update_or_create(
-                    id=conversation.entity.id,
-                    defaults={
-                        'username': conversation.entity.username,
-                        'first_name': conversation.entity.first_name,
-                        'last_name': conversation.entity.last_name,
-                        'phone_number': conversation.entity.phone
-                    }
-                )
-            # time.sleep(0.5)
-            bar()
+                # time.sleep(0.5)
+                bar()
+            except Exception as e:
+                print(e)
 
 
 def get_users_in_group(client: TelegramClient):
@@ -56,19 +59,23 @@ def get_users_in_group(client: TelegramClient):
         users = client.get_participants(group.id)
         with alive_bar(len(users), title=group.title) as bar:
             for user in users:
-                if user.bot != True:
-                    TelegramUser.objects.update_or_create(
-                        id=user.id,
-                        defaults={
-                            'username': user.username,
-                            'first_name': user.first_name,
-                            'last_name': user.last_name,
-                            'phone_number': user.phone
-                        }
-                    )
-                    group.members.add(user.id)
-                    # time.sleep(0.5)
+                try:
+                    if user.bot != True:
+                        TelegramUser.objects.update_or_create(
+                            id=user.id,
+                            defaults={
+                                'username': user.username,
+                                'first_name': user.first_name,
+                                'last_name': user.last_name,
+                                'phone_number': user.phone
+                            }
+                        )
+                        group.members.add(user.id)
+                        # time.sleep(0.3)
                     bar()
+                except Exception as e:
+                    print(e)
+
 
 def get_users_in_group_with_message(client: TelegramClient):
     groups = TelegramGroup.objects.all()
